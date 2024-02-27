5619d16a360962c799adc22392311b66c6bd37c1
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2024-02-27 20:18:55 +1100

    add example button typescript component that displays tag selected data, getting data from endpoint, typescript data validation

--- src/components/APICardTest.tsx
+++ src/components/APICardTest.tsx
@@ -1,4 +1,36 @@
+// a live api endpoint on a static site
+import { useState } from "react";
+
+type Data = {
+    id: string;
+    slug: string;
+    body: string;
+    collection: "tags";
+    data: {
+        selected: string;
+        color: string;
+        location: string;
+        annotation: string;
+        book: string;
+    }
+};
+
+
 const APICardTest = () => {
-    return <div>APICardTest</div>;
+    const [tags, setTags] = useState([])
+
+    const handleClick = async () => {
+        const res = await fetch("/endpoint.json");
+        const data = await res.json();
+        setTags(data);
+        console.log("Data====>>>", data)
+    }
+    return <div>
+        <p>Get more items</p>
+        {tags.length > 0 && tags.map((tagsData: Data) => <p key={tagsData.id} >{tagsData.data.selected}</p>)
+
+        }
+        <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Get it</button>
+    </div>;
 };
 export default APICardTest;
