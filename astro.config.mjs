ce1ae15b1d8999febf0b4b2f6d8dc820c501f3ea
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2024-02-27 19:18:38 +1100

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import netlify from "@astrojs/netlify/functions";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), react()],
   output: "server",
   adapter: netlify()
 });
