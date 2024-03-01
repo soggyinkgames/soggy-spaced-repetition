4398709d87feddbf4b6b09e2c5a7d1578e1b0c6a
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2024-03-01 13:12:43 +1100

    customise tailwind theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'accent': '#f97316',
+				'primary': '#22d3ee',
+				'secondary': '#e879f9',
+				gray: {
+					100: '#ede9fe',
+					200: '#ddd6fe',
+					300: '#c4b5fd',
+					400: '#a78bfa',
+					500: '#8b5cf6',
+					600: '#7c3aed',
+					700: '#6d28d9',
+					800: '#5b21b6',
+					900: '#2e1065',
+				},
+			},
+		},
 	},
 	plugins: [require('@tailwindcss/typography')],
 }
